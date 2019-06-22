4c196a24da62ce0e88daca765222807b23f7aa12
coderquill <coderquill@gmail.com>
2019-06-22 12:20:27 +0530

    longest sum contiguous array

--- Dynamic programming/Kadane.java
+++ Dynamic programming/Kadane.java
@@ -2,8 +2,21 @@
 import java.util.*;
 
 class Kadane{
-	Public static void main(String args[]){
+	public static int lis(int[] a, int n){
+		int meh= 0, msf = 0;
+		for(int i = 0; i<n; i++){
+			meh = meh+a[i];
+			if(meh<0)
+				meh = 0;
+			if(msf<meh)
+				msf = meh;
+		}
+		return msf;
+	}
+	public static void main(String args[]){
 		Scanner scan = new Scanner(System.in);
-		int[] array = new int[n];
+		int[] array = {8,2,-7,6,-2};
+		System.out.println(lis(array,array.length));
+		
 	}
 }
